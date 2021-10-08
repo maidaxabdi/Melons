595ed1139951384c1871559ed952eb98fb5e3d88
maidaxabdi <maidaxabdi@gmail.com>
2021-10-08 12:02:15 -0500

    Finished Part 1

---
+++ melons.py
@@ -0,0 +1,47 @@
+
+
+"""Classes for melon orders."""
+
+class AbstractMelonOrder():
+    """An abstract base class that other Melon Orders inherit"""
+
+    def __init__(self, species, qty, order_type, tax):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
+
+
+    def get_total(self):
+        """Calculate price, including tax."""
+        base_price = 5 * 1.5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+        
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""    
+    def __init__(self, species, qty):
+        #"""Initialize melon order attributes."""
+       super().__init__(species, qty, "domestic", 0.08)
+    
+
+class InternationalMelonOrder(AbstractMelonOrder):
+       # """An international (non-US) melon order."""
+    
+    
+    def __init__(self, species, qty, country_code):
+       # """Initialize melon order attributes."""
+        super().__init__(species, qty, "international", 0.17)
+        self.country_code = country_code
+     
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
